39115303e586551416a0714c14544ce88a08b91f
Tom Collingwood <tomcoll@hotmail.co.uk>
2020-02-01 12:33:16 +0000

    Starting Turn on and off again. Big image files, sorry.

---
+++ Repair/Frames/TurnItOnAndOffFrame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
+using Nez.Sprites;
+
+namespace Repair.Frames
+{
+    public class TurnItOnAndOffFrame : Frame
+    {
+        private Entity computerEntity, buttonEntity, loadingIconEntity, errorEntity, profileEntity;
+        public TurnItOnAndOffFrame()
+        {
+            var scene = Core.Scene;
+            var computer = scene.Content.Load<Texture2D>("../assets/computer.png");
+            computerEntity = scene.CreateEntity("computer");
+            computerEntity.AddComponent(new SpriteRenderer(computer));
+            computerEntity.Transform.Position = new Vector2(400, 400);
+
+            var button = scene.Content.Load<Texture2D>("../assets/power.png");
+            buttonEntity = scene.CreateEntity("button");
+            buttonEntity.AddComponent(new SpriteRenderer(button));
+            buttonEntity.Transform.Position = new Vector2(400, 400);
+
+            var loadingIcon = scene.Content.Load<Texture2D>("../assets/loading.png");
+            loadingIconEntity = scene.CreateEntity("computer");
+            loadingIconEntity.AddComponent(new SpriteRenderer(loadingIcon));
+            loadingIconEntity.Transform.Position = new Vector2(400, 400);
+
+            var error = scene.Content.Load<Texture2D>("../assets/error.png");
+            errorEntity = scene.CreateEntity("error");
+            errorEntity.AddComponent(new SpriteRenderer(error));
+            errorEntity.Transform.Position = new Vector2(400, 400);
+
+            var profile = scene.Content.Load<Texture2D>("../assets/profile.png");
+            profileEntity = scene.CreateEntity("computer");
+            profileEntity.AddComponent(new SpriteRenderer(profile));
+            profileEntity.Transform.Position = new Vector2(400, 400);
+        }
+    }
+}
