0deae5e6514345c1be744de43e14b640e7d96a66
Tom Collingwood <tomcoll@hotmail.co.uk>
2020-02-02 13:20:23 +0000

    Installed score frame

--- Repair/Framework/FrameSequencer.cs
+++ Repair/Framework/FrameSequencer.cs
@@ -24,18 +24,15 @@ namespace Framework
             }
             else if (frames.Count == 0)
             {
-                return new TitleFrame("title_died");
+                LoadFrameSequence();
             }
             return frames.Dequeue();
+
         }
 
         private void LoadFrameSequence()
         {
-
-            frames.Enqueue(new PaintingBalanceFrame());
-
             frames.Enqueue(new TitleFrame("title_repair"));
-
             frames.Enqueue(new TitleFrame("title_job_handiman"));
             frames.Enqueue(new TitleFrame("title_hangpainting"));
             frames.Enqueue(new PaintingBalanceFrame());
@@ -50,7 +47,6 @@ namespace Framework
             frames.Enqueue(new WheelAlignmentFrame());
             frames.Enqueue(new TitleFrame("title_replacelights"));
             frames.Enqueue(new ReplaceLightsFrame());
-
             frames.Enqueue(new TitleFrame("job_it"));
             frames.Enqueue(new TitleFrame("title_replaceparts"));
             frames.Enqueue(new ReplacePartFrame());
@@ -58,13 +54,13 @@ namespace Framework
             frames.Enqueue(new DustComputerFrame());
             frames.Enqueue(new TitleFrame("title_restart"));
             frames.Enqueue(new TurnItOnAndOffFrame());
-
             frames.Enqueue(new TitleFrame("title_job_doctor"));
             frames.Enqueue(new TitleFrame("title_heart"));
             frames.Enqueue(new HeartSurgeryFrame());
             frames.Enqueue(new RepairHeartFrame());
             frames.Enqueue(new TitleFrame("title_retire"));
             frames.Enqueue(new TitleFrame("title_died"));
+            frames.Enqueue(new ScoreFrame());
         }
     }
 }
